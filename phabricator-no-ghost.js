260fbc3317d196550147550e52cad0059e7763a6
Greg Tatum <tatum.creative@gmail.com>
2019-09-05 09:41:44 -0500

    Add phabricator script

---
+++ phabricator-no-ghost.js
@@ -0,0 +1,27 @@
+// ==UserScript==
+// @name     Phabricator - Make it readable
+// @version  1
+// @grant    none
+// @include  https://phabricator.services.mozilla.com/*
+// @run-at   document-start
+// ==/UserScript==
+
+// https://wiki.greasespot.net/Greasemonkey_Manual:API
+
+addStylesheet(`
+  /* Make the ghosted inline elements visible */
+  .differential-inline-comment.inline-comment-ghost {
+    opacity: 1 !important;
+  }
+
+	.differential-inline-comment.inline-comment-ghost .differential-inline-done-label, .differential-inline-comment.inline-comment-ghost {
+		color: #000;
+  }
+`);
+
+function addStylesheet(text) {
+  const style = document.createElement('style');
+  style.innerHTML = text
+  document.head.appendChild(style);
+  console.log('GreaseMonkey: adding stylesheet', style)
+}
